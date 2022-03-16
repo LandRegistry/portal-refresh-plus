9fb3dcb0afec6e9f9d54b9f750da4b668cb21155
lauren-dowling-designer <laurenj.dowling@gmail.com>
2022-03-16 18:36:10 +0000

    select checkboxes for first search

--- app/assets/javascripts/Official-copies-documents.js
+++ app/assets/javascripts/Official-copies-documents.js
@@ -1,48 +1,80 @@
-function selectDocument(checkboxID, tablerowID){
+
+var basketQuantity = 0;
+
+function anythingInBasket(){
+    if(basketQuantity === 0){
+        document.getElementById("basketCounter").style.display = 'none';
+    }
+    if(basketQuantity >= 1){
+        document.getElementById("basketCounter").style.display = 'block';
+    }
+}
+
+function selectDocument(checkboxID, tablerowID, selectAllID){
 
     if (document.getElementById(checkboxID).checked) {
 
         var DocumentAdded = document.getElementById(tablerowID);
         DocumentAdded.style.display = 'table-row';
+        basketQuantity += 1;
+        anythingInBasket();
+        document.getElementById("basketCounter").innerHTML = basketQuantity;
+
     }   
     else{
+        console.log('code executing');
         var DocumentAdded = document.getElementById(tablerowID);
         DocumentAdded.style.display = 'none';
+        basketQuantity -= 1;
+        anythingInBasket();
+        document.getElementById("basketCounter").innerHTML = basketQuantity;
+        if (document.getElementById(selectAllID).checked) {
+            document.getElementById(selectAllID).checked = false;
+        }
+
     }
 }
 
 function SelectAll(whichSelectAll){
-    console.log('function being called');
+    // console.log('function being called');
     if(whichSelectAll === '27a'){
         if(document.getElementById('27a-checkbox-all').checked){
-            console.log('Select all for 27a Drakefell Road has been selected');
-            document.getElementById('27a-checkbox-1').checked = true;
-            selectDocument('27a-checkbox-1', '27a-register')
-            document.getElementById('27a-checkbox-2').checked = true;
-            selectDocument('27a-checkbox-2', '27a-titlePlan')
+            // console.log('Select all for 27a Drakefell Road has been selected');
+            if(document.getElementById('27a-checkbox-1').checked == false){
+                document.getElementById('27a-checkbox-1').checked = true;
+                selectDocument('27a-checkbox-1', '27a-register', '27a-checkbox-all');
+            }
+            if(document.getElementById('27a-checkbox-2').checked == false){
+                document.getElementById('27a-checkbox-2').checked = true;
+                selectDocument('27a-checkbox-2', '27a-titlePlan', '27a-checkbox-all');
+            }
         }
         else{
-            console.log('Select all for 27a Drakefell Road has been deselected');
+            // console.log('Select all for 27a Drakefell Road has been deselected');
             document.getElementById('27a-checkbox-1').checked = false;
-            selectDocument('27a-checkbox-1', '27a-register')
             document.getElementById('27a-checkbox-2').checked = false;
-            selectDocument('27a-checkbox-2', '27a-titlePlan')
+            selectDocument('27a-checkbox-2', '27a-titlePlan', '27a-checkbox-all');
+            selectDocument('27a-checkbox-1', '27a-register', '27a-checkbox-all');
         }
     }
     if(whichSelectAll === '27b'){
         if(document.getElementById('27b-checkbox-all').checked){
             console.log('Select all for 27b Drakefell Road has been selected');
-            document.getElementById('27b-checkbox-1').checked = true;
-            selectDocument('27b-checkbox-1', '27b-register');
-            document.getElementById('27b-checkbox-2').checked = true;
-            selectDocument('27b-checkbox-2', '27b-titlePlan');
+            if(document.getElementById('27b-checkbox-1').checked == false){
+                document.getElementById('27b-checkbox-1').checked = true;
+                selectDocument('27b-checkbox-1', '27b-register', '27b-checkbox-all');
+            }
+            if(document.getElementById('27b-checkbox-2').checked == false){
+                document.getElementById('27b-checkbox-2').checked = true;
+                selectDocument('27b-checkbox-2', '27b-titlePlan', '27b-checkbox-all');
+            }
         }
         else{
             console.log('Select all for 27b Drakefell Road has been deselected');
             document.getElementById('27b-checkbox-1').checked = false;
-            selectDocument('27b-checkbox-1', '27b-register');
+            selectDocument('27b-checkbox-1', '27b-register', '27b-checkbox-all');
             document.getElementById('27b-checkbox-2').checked = false;
-            selectDocument('27b-checkbox-2', '27b-titlePlan');
+            selectDocument('27b-checkbox-2', '27b-titlePlan', '27b-checkbox-all');
         }
     }
 }
