562615b218fc41f81740ef70fe995516b1716a99
Andy Carr <hello@andycarr.design>
2022-04-07 00:18:02 +0100

    Andy JS

--- app/assets/javascripts/Official-copies-documents.js
+++ app/assets/javascripts/Official-copies-documents.js
@@ -436,6 +436,106 @@ function SelectAll(whichSelectAll){
 
         }
     }
+    if(whichSelectAll === '144a'){
+        if(document.getElementById('144a-checkbox-all').checked){
+            console.log('Select all for 144a Drakefell Road has been selected');
+            if(document.getElementById('144a-checkbox-1').checked == false){
+                document.getElementById('144a-checkbox-1').checked = true;
+                selectDocument('144a-checkbox-1', '144a-register', '144a-checkbox-all', '144a Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Register', '144aDIV', '144aLIST', 'Register3LI', '144a_RegisterIcon');
+            }
+            if(document.getElementById('144a-checkbox-2').checked == false){
+                document.getElementById('144a-checkbox-2').checked = true;
+                selectDocument('144a-checkbox-2', '144a-titlePlan', '144a-checkbox-all', '144a Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Title plan', '144aDIV', '144aLIST', 'TitlePlan3LI', '144a_TitlePlanIcon');
+            }
+            sessionStorage.setItem('144a-checkbox-all', true);
+    
+        }
+        else{
+            console.log('Select all for 144a Drakefell Road has been deselected');
+            document.getElementById('144a-checkbox-1').checked = false;
+            selectDocument('144a-checkbox-1', '144a-register', '144a-checkbox-all', '144a Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Register', '144aDIV', '144aLIST', 'Register3LI', '144a_RegisterIcon');
+            document.getElementById('144a-checkbox-2').checked = false;
+            selectDocument('144a-checkbox-2', '144a-titlePlan', '144a-checkbox-all', '144a Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Title plan', '144aDIV', '144aLIST', 'TitlePlan3LI', '144a_TitlePlanIcon');
+    
+            sessionStorage.removeItem('144a-checkbox-all');
+    
+        }
+    }
+    if(whichSelectAll === '144b'){
+        if(document.getElementById('144b-checkbox-all').checked){
+            console.log('Select all for 144b Drakefell Road has been selected');
+            if(document.getElementById('144b-checkbox-1').checked == false){
+                document.getElementById('144b-checkbox-1').checked = true;
+                selectDocument('144b-checkbox-1', '144b-register', '144b-checkbox-all', '144b Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Register', '144bDIV', '144bLIST', 'Register4LI', '144b_RegisterIcon');
+            }
+            if(document.getElementById('144b-checkbox-2').checked == false){
+                document.getElementById('144b-checkbox-2').checked = true;
+                selectDocument('144b-checkbox-2', '144b-titlePlan', '144b-checkbox-all', '144b Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Title plan', '144bDIV', '144bLIST', 'TitlePlan4LI', '144b_TitlePlanIcon');
+            }
+            sessionStorage.setItem('144b-checkbox-all', true);
+    
+        }
+        else{
+            console.log('Select all for 144b Drakefell Road has been deselected');
+            document.getElementById('144b-checkbox-1').checked = false;
+            selectDocument('144b-checkbox-1', '144b-register', '144b-checkbox-all', '144b Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Register', '144bDIV', '144bLIST', 'Register4LI', '144b_RegisterIcon');
+            document.getElementById('144b-checkbox-2').checked = false;
+            selectDocument('144b-checkbox-2', '144b-titlePlan', '144b-checkbox-all', '144b Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Title plan', '144bDIV', '144bLIST', 'TitlePlan4LI', '144b_TitlePlanIcon');
+    
+            sessionStorage.removeItem('144b-checkbox-all');
+    
+        }
+    }
+    if(whichSelectAll === '144c'){
+        if(document.getElementById('144c-checkbox-all').checked){
+            console.log('Select all for 144c Drakefell Road has been selected');
+            if(document.getElementById('144c-checkbox-1').checked == false){
+                document.getElementById('144c-checkbox-1').checked = true;
+                selectDocument('144c-checkbox-1', '144c-register', '144c-checkbox-all', '144c Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Register', '144cDIV', '144cLIST', 'Register5LI', '144c_RegisterIcon');
+            }
+            if(document.getElementById('144c-checkbox-2').checked == false){
+                document.getElementById('144c-checkbox-2').checked = true;
+                selectDocument('144c-checkbox-2', '144c-titlePlan', '144c-checkbox-all', '144c Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Title plan', '144cDIV', '144cLIST', 'TitlePlan5LI', '144c_TitlePlanIcon');
+            }
+            sessionStorage.setItem('144c-checkbox-all', true);
+    
+        }
+        else{
+            console.log('Select all for 144c Drakefell Road has been deselected');
+            document.getElementById('144c-checkbox-1').checked = false;
+            selectDocument('144c-checkbox-1', '144c-register', '144c-checkbox-all', '144c Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Register', '144cDIV', '144cLIST', 'Register5LI', '144c_RegisterIcon');
+            document.getElementById('144c-checkbox-2').checked = false;
+            selectDocument('144c-checkbox-2', '144c-titlePlan', '144c-checkbox-all', '144c Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Title plan', '144cDIV', '144cLIST', 'TitlePlan5LI', '144c_TitlePlanIcon');
+    
+            sessionStorage.removeItem('144c-checkbox-all');
+    
+        }
+    }
+    if(whichSelectAll === '144d'){
+        if(document.getElementById('144d-checkbox-all').checked){
+            console.log('Select all for 144d Drakefell Road has been selected');
+            if(document.getElementById('144d-checkbox-1').checked == false){
+                document.getElementById('144d-checkbox-1').checked = true;
+                selectDocument('144d-checkbox-1', '144d-register', '144d-checkbox-all', '144d Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Register', '144dDIV', '144dLIST', 'Register6LI', '144d_RegisterIcon');
+            }
+            if(document.getElementById('144d-checkbox-2').checked == false){
+                document.getElementById('144d-checkbox-2').checked = true;
+                selectDocument('144d-checkbox-2', '144d-titlePlan', '144d-checkbox-all', '144d Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Title plan', '144dDIV', '144dLIST', 'TitlePlan6LI', '144d_TitlePlanIcon');
+            }
+            sessionStorage.setItem('144d-checkbox-all', true);
+    
+        }
+        else{
+            console.log('Select all for 144d Drakefell Road has been deselected');
+            document.getElementById('144d-checkbox-1').checked = false;
+            selectDocument('144d-checkbox-1', '144d-register', '144d-checkbox-all', '144d Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Register', '144dDIV', '144dLIST', 'Register6LI', '144d_RegisterIcon');
+            document.getElementById('144d-checkbox-2').checked = false;
+            selectDocument('144d-checkbox-2', '144d-titlePlan', '144d-checkbox-all', '144d Drakefell Road, Coventry, CV5K2X', 'NN890234', 'Title plan', '144dDIV', '144dLIST', 'TitlePlan6LI', '144d_TitlePlanIcon');
+    
+            sessionStorage.removeItem('144d-checkbox-all');
+    
+        }
+    }
 }
 
 
